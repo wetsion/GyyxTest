7529f6d37d4d7880727da08d09da34e615b1b95f
wetsion <13110018525@163.com>
2017-04-14 20:49:13 +0800

    第一次测试用例为bbbbb

--- GuangYuTest3/src/test/Main.java
+++ GuangYuTest3/src/test/Main.java
@@ -33,7 +33,7 @@ public class Main {
         return maxLength;  
     }  
     public static void main(String[] args) {  
-        String str="abcabcbb";  
+        String str="bbbbb";  
         System.out.println(maxLength(str));  
   
     }  
